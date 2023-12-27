f6541eb33ba8bde019bdab2f7f55e21cafc60f07
stabondar <stabondar@gmail.com>
2023-12-27 15:26:13 +0200

    remove map

--- src/moduls/Pages/Contact/index.js
+++ src/moduls/Pages/Contact/index.js
@@ -1,11 +1,9 @@
 import { CurrentPage } from "../../Components/CurrentPage";
-import Map from "./js/Map";
 
 export default class index
 {
     constructor()
     {
         CurrentPage()
-        this.map = new Map()
     }
 }
